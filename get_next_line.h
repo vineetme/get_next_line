a6ace46f70a3c5160a96807ad8ac61b4bade8579
Vineet Meharia <vmeharia@c4a11c4.42berlin.de>
2026-06-03 15:54:09 +0200

    fixed norm error in header

--- get_next_line.h
+++ get_next_line.h
@@ -1,11 +1,24 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.h                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: vmeharia <vmeharia@student.42berlin.d      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/06/03 15:51:08 by vmeharia          #+#    #+#             */
+/*   Updated: 2026/06/03 15:53:31 by vmeharia         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #ifndef GET_NEXT_LINE_H
-#define GET_NEXT_LINE_H
+# define GET_NEXT_LINE_H
 
 # include <unistd.h>
 # include <stdlib.h>
 
 # ifndef BUFFER_SIZE
-# define BUFFER_SIZE 42
+#  define BUFFER_SIZE 42
+
 # endif
 
 size_t		ft_strlen(const char *str);
@@ -16,6 +29,5 @@ char		*ft_strjoin(char *stash, char *buf);
 size_t		ft_strlcpy(char *dst, char *src, size_t dstsize);
 size_t		ft_strlcat(char *dst, char *src, size_t dstsize);
 char		*ft_strchr(char *str, int c);
-
 
 #endif
