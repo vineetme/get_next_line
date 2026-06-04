548a71cf67f36d6e2c9d2d9c9a285baac41d993b
Vineet Meharia <vmeharia@c4a11c4.42berlin.de>
2026-06-04 17:30:35 +0200

    updated utils

--- get_next_line_utils.c
+++ get_next_line_utils.c
@@ -6,7 +6,7 @@
 /*   By: vmeharia <vmeharia@student.42berlin.d      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/06/02 11:12:58 by vmeharia          #+#    #+#             */
-/*   Updated: 2026/06/03 15:49:41 by vmeharia         ###   ########.fr       */
+/*   Updated: 2026/06/03 18:04:36 by vmeharia         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -25,8 +25,8 @@ size_t	ft_strlen(const char *str)
 char	*ft_strjoin(char *stash, char *buf)
 {
 	char	*ptr;
-	size_t	len1;
-	size_t	len2;
+	size_t	i;
+	size_t	j;
 
 	if (!stash)
 	{
@@ -35,62 +35,20 @@ char	*ft_strjoin(char *stash, char *buf)
 			return (NULL);
 		stash[0] = '\0';
 	}
-	len1 = ft_strlen(stash);
-	len2 = ft_strlen(buf);
-	ptr = malloc (len1 + len2 + 1);
+	ptr = malloc(ft_strlen(stash) + ft_strlen(buf) + 1);
 	if (!ptr)
-	{
-		free (stash);
-		return (NULL);
-	}
-	ft_strlcpy(ptr, stash, len1 + 1);
-	ft_strlcat(ptr, buf, len1 + len2 + 1);
-	free (stash);
-	return (ptr);
-}
-
-size_t	ft_strlcpy(char *dst, char *src, size_t dstsize)
-{
-	size_t	i;
-
+		return (free(stash), NULL);
 	i = 0;
-	if (dstsize != 0)
+	while (stash[i])
 	{
-		while (i < (dstsize - 1) && src[i] != '\0')
-		{
-			dst[i] = src[i];
-			i++;
-		}
-		dst[i] = '\0';
+		ptr[i] = stash[i];
+		i++;
 	}
-	return (ft_strlen(src));
-}
-
-size_t	ft_strlcat(char *dst, char *src, size_t dstsize)
-{
-	size_t	dst_len;
-	size_t	src_len;
-	size_t	j;
-
-	src_len = ft_strlen(src);
 	j = 0;
-	dst_len = 0;
-	while (dst_len < dstsize && dst[dst_len] != '\0')
-		dst_len++;
-	if (dstsize == 0)
-		return (src_len);
-	if (dstsize == dst_len)
-		return (dstsize + src_len);
-	else
-	{
-		while ((dstsize - 1) > (dst_len + j) && src[j] != '\0')
-		{
-			dst[dst_len + j] = src[j];
-			j++;
-		}
-		dst[dst_len + j] = '\0';
-		return (dst_len + src_len);
-	}
+	while (buf[j])
+		ptr[i++] = buf[j++];
+	ptr[i] = '\0';
+	return (free(stash), ptr);
 }
 
 char	*ft_strchr(char *str, int c)
